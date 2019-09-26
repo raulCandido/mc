922ea6d1007cda7c6689f2ba0ba6cdc8b24e49bf
Raul Candido <raul.cand@hotmail.com>
2019-09-26 18:48:10 -0300

    Initial commit

---
+++ src/main/java/com/mc/McApplication.java
@@ -0,0 +1,13 @@
+package com.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McApplication.class, args);
+	}
+
+}
