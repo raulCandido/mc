bc9cf58f9ff46c543f4d017dcb1db502e46e75e8
Raul Candido <raul.cand@hotmail.com>
2019-09-26 21:08:44 -0300

    Criando novo dominio e metodo para insert de informacoes
    Criando novo dominio e metodo para insert de informacoes assim que o servidor sobe.

--- src/main/java/com/mc/McApplication.java
+++ src/main/java/com/mc/McApplication.java
@@ -1,13 +1,32 @@
 package com.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mc.domain.Categoria;
+import com.mc.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class McApplication {
+public class McApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "DBA");
+		
+		repository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
