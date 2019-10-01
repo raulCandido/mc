853acbc3f0b9461206a81f8a0139fefa9aa1f086
Raul Candido <raul.cand@hotmail.com>
2019-09-30 21:56:37 -0300

    Implementacao de novas classes de dominio
    Implementacao das classes: cidade, cliente, endereco, tipoCliente, estado.
    
    Implementacao dos repository: cidade e estado.

--- src/main/java/com/mc/McApplication.java
+++ src/main/java/com/mc/McApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mc.domain.Categoria;
+import com.mc.domain.Cidade;
+import com.mc.domain.Estado;
 import com.mc.domain.Produto;
 import com.mc.repository.CategoriaRepository;
+import com.mc.repository.CidadeRepository;
+import com.mc.repository.EstadoRepository;
 import com.mc.repository.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class McApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -39,8 +47,20 @@ public class McApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 
+		Estado est1 = new Estado(null, "Minhas Gerais");
+		Estado est2 = new Estado(null, "Ceará");
+
+		Cidade cid1 = new Cidade(null, "Uberlândia", est1);
+		Cidade cid2 = new Cidade(null, "Caucaia", est2);
+		Cidade cid3 = new Cidade(null, "Fortaleza", est2);
+
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 	}
 
 }
---
+++ src/main/java/com/mc/domain/enums/TipoCliente.java
@@ -0,0 +1,34 @@
+package com.mc.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"), PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+	private int codigo;
+	private String descricao;
+
+	private TipoCliente(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static TipoCliente toEnum(Integer codigo) {
+		if(codigo == null) {
+			return null;
+		}
+		for (TipoCliente tipoCliente : TipoCliente.values()) {
+			if(codigo.equals(tipoCliente.getCodigo())) {
+				return tipoCliente;
+			}
+		}
+		throw new IllegalArgumentException("ID inválido: "+ codigo);
+	}
+}
