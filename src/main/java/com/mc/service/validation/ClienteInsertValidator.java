683a45cb793fab7f5d78b679518d37b31143b039
Raul Candido <raul.cand@hotmail.com>
2019-10-29 21:49:56 -0300

    Atualizações para validar a unicidade do CPF e email do cliente.

--- src/main/java/com/mc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/mc/service/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mc.domain.Cliente;
 import com.mc.domain.enums.TipoCliente;
 import com.mc.dto.ClienteNewDTO;
+import com.mc.repository.ClienteRepository;
 import com.mc.resource.exception.FieldMessage;
 import com.mc.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public void initialize(ClienteInsert constraintAnnotation) {
 		// TODO Auto-generated method stub
@@ -29,6 +36,17 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (value.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(value.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = repository.findByCpfOuCnpj(value.getCpfOuCnpj());
+		
+		if (cliente != null) {
+			if (cliente.getEmail() != null) {
+				list.add(new FieldMessage("email", "Email: " +cliente.getEmail()+ " já cadastrado."));
+			}
+			if (cliente.getCpfOuCnpj() != null) {
+				list.add(new FieldMessage("cpfOuCnpj", "CPF/CNPJ já cadastrado."));
+			}
 		}
 		
 		for (FieldMessage fieldMessage : list) {
