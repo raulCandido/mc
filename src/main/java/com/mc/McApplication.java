bb78cc46d86795039604ee9270e8c611cdeeda4b
Raul Candido <raul.cand@hotmail.com>
2020-03-27 16:24:11 -0300

    Aplicacao consumindo servicos da aws

--- src/main/java/com/mc/McApplication.java
+++ src/main/java/com/mc/McApplication.java
@@ -1,13 +1,26 @@
 package com.mc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mc.service.S3Service;
+
 @SpringBootApplication
-public class McApplication {
+public class McApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\foto\\yodinha.jpeg");
+		//s3Service.uploadFile("C:\\foto\\TESTE.png");
 	}
 
 }
--- src/main/java/com/mc/service/S3Service.java
+++ src/main/java/com/mc/service/S3Service.java
@@ -2,12 +2,14 @@ package com.mc.service;
 
 import java.io.File;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
@@ -15,22 +17,30 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 @Service
 public class S3Service {
 	
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+
 	@Autowired
 	private AmazonS3 s3Client;
 	
 	@Value("${s3.bucket}")
 	private String bucketName;
 	
-	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
 	
 	public void uploadFile(String localFilePath) {
 		
 		try {
 			File file = new File(localFilePath);
-			s3Client.putObject(new PutObjectRequest(bucketName, "teste", file));
+			
+			LOG.info("Iniciando upload");
+			s3Client.putObject(new PutObjectRequest(bucketName, file.getName(), file));
+			LOG.info("Finalizado upload");
 		} catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException :"+ e.getMessage());
+			LOG.info("AmazonServiceException: "+ e.getMessage());
 			LOG.info("Status code: "+ e.getErrorCode());
+		}catch (AmazonClientException e) {
+			LOG.info("AmazonClientException: "+ e.getMessage());
 		}
 	}
+
 }
+
